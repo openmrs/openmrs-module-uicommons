89334ba01d734c814f211a8daf23f68614a07ac1
djazayeri <jazayeri@alum.mit.edu>
2013-04-09 11:04:02 -0700

    Cleaning up uicommons module so that Glauber and Natalia can start refactoring SCSS code there

--- api/src/main/java/org/openmrs/module/uicommons/UicommonsActivator.java
+++ api/src/main/java/org/openmrs/module/uicommons/UicommonsActivator.java
@@ -14,57 +14,11 @@
 package org.openmrs.module.uicommons;
 
 
-import org.apache.commons.logging.Log; 
-import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.BaseModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
-public class UicommonsActivator implements ModuleActivator {
+public class UicommonsActivator extends BaseModuleActivator {
 	
-	protected Log log = LogFactory.getLog(getClass());
-		
-	/**
-	 * @see ModuleActivator#willRefreshContext()
-	 */
-	public void willRefreshContext() {
-		log.info("Refreshing uicommons Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#contextRefreshed()
-	 */
-	public void contextRefreshed() {
-		log.info("uicommons Module refreshed");
-	}
-	
-	/**
-	 * @see ModuleActivator#willStart()
-	 */
-	public void willStart() {
-		log.info("Starting uicommons Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#started()
-	 */
-	public void started() {
-		log.info("uicommons Module started");
-	}
-	
-	/**
-	 * @see ModuleActivator#willStop()
-	 */
-	public void willStop() {
-		log.info("Stopping uicommons Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#stopped()
-	 */
-	public void stopped() {
-		log.info("uicommons Module stopped");
-	}
-		
 }
