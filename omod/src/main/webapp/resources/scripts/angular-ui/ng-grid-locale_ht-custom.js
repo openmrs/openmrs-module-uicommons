8a372d6507d0c3a48b372f7c534ff420e7644bb1
mogoodrich <mgoodrich@pih.org>
2014-04-29 14:20:22 -0400

    added custom Creole localization file for ng-grid angular element (still needs actual translations)

---
+++ omod/src/main/webapp/resources/scripts/angular-ui/ng-grid-locale_ht-custom.js
@@ -0,0 +1,7 @@
+
+// our custom Creole localizations for the ng-grid element
+window.ngGrid.i18n['ht'] = {
+    ngTotalItemsLabel: "[translate to Creole]:",
+    ngPageSizeLabel: "[translate to Creole]:"
+
+};
