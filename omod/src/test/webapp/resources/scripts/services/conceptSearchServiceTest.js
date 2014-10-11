519a2ddc4e2c8ff5621eac300dddf6e73ebd4e08
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-10-10 17:58:29 -0700

    UICM-36 - Angular service for ConceptSearch

---
+++ omod/src/test/webapp/resources/scripts/services/conceptSearchServiceTest.js
@@ -0,0 +1,37 @@
+describe('ConceptSearchService tests', function() {
+
+    var conceptSearchService;
+    var q;
+    var deferred;
+
+    beforeEach(module('conceptSearchService'));
+
+    // create mock ConceptSearch resource
+    var mock = jasmine.createSpyObj('ConceptSearch', ['query']);
+    mock.query.andCallFake(function() {
+
+        deferred = q.defer();
+
+        var promise_mock = {
+            $promise: deferred.promise
+        };
+
+        return promise_mock;
+    });
+
+    beforeEach(module(function($provide) {
+        $provide.value('ConceptSearch', mock);
+    }));
+
+    // inject necessary dependencies
+    beforeEach(inject(function (_ConceptSearchService_,$q) {
+        conceptSearchService = _ConceptSearchService_;
+        q = $q;
+    }));
+
+    it('should call ConceptSearch resource with query value', function() {
+        conceptSearchService.search("abc", { v: "full" });
+        expect(mock.query).toHaveBeenCalledWith({ q: 'abc', v: "full" });
+    });
+
+});
