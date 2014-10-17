de778a593aae9658b02f5b3d72b2d48e16eca82f
wyclif <wyclif@openmrs.org>
2014-10-16 20:41:06 -0400

    The typeahead input should clear itself if user exists it without making a selection

--- omod/src/main/webapp/resources/scripts/directives/coded-or-free-text-answer.js
+++ omod/src/main/webapp/resources/scripts/directives/coded-or-free-text-answer.js
@@ -85,8 +85,14 @@ angular.module('uicommons.widget.coded-or-free-text-answer', [ 'conceptSearchSer
                         $scope.ngModel = withoutName;
                     }
                 }
+
+                $scope.verify = function(){
+                    if(!$scope.ngModel){
+                        $('#'+$scope.inputId).val('');
+                    }
+                }
             },
-            template: '<input type="text" id="{{ inputId }}" ng-model="ngModel" ' +
+            template: '<input type="text" id="{{ inputId }}" ng-model="ngModel" ng-blur="verify()" ' +
                 'typeahead="result as format(result) for result in search($viewValue) | filter:$viewValue" ' +
                 'typeahead-editable="false" typeahead-on-select="onSelect($item, $model, $label)" autocomplete="off" ' +
                 'typeahead-wait-ms="20" typeahead-min-length="2" style="min-width: 500px" />'
