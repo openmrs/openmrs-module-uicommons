04e637f12f823d517416aeb7d97ed0fd59968830
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-10-26 14:59:10 +0530

    UICM-44 - Angular widget for selecting an OrderFrequency

---
+++ omod/src/main/webapp/resources/scripts/directives/select-order-frequency.js
@@ -0,0 +1,49 @@
+angular.module('uicommons.widget.select-order-frequency', [ 'ui.bootstrap' ])
+
+    .directive('selectOrderFrequency', [ function() {
+
+        return {
+            restrict: 'E',
+            scope: {
+                ngModel: '=',
+                id: '@',
+                locale: '@',
+                placeholder: '@',
+                frequencies: '&'
+            },
+            link: function($scope, element, attrs) {
+                $scope.required = attrs.hasOwnProperty('required'); // required attribute has no value
+                $scope.inputId = emr.domId($scope.id, 'sel-freq', 'input');
+
+                var options = [];
+                _.each($scope.frequencies(), function(frequency) {
+                    var concept = frequency.concept;
+                    _.each(concept.names, function(name) {
+                        if (emr.compatibleLocaleForSession(name.locale)) {
+                            var display = name.name === concept.display ?
+                                name.name :
+                                name.name + " &rarr; " + concept.display;
+                            options.push({
+                                display: display,
+                                searchOn: name.name,
+                                frequency: frequency
+                            });
+                        }
+                    });
+                });
+                $scope.options = _.sortBy(options, function(item) {
+                    return item.display.length;
+                });
+
+                $scope.verify = function() {
+                    if(!$scope.ngModel){
+                        $('#'+$scope.inputId).val('');
+                    }
+                }
+            },
+            template: '<input type="text" id="{{ inputId }}" ng-model="ngModel" ng-blur="verify()" ' +
+                'ng-required="required" ' +
+                'typeahead="opt.frequency as opt.display for opt in options | filter:{searchOn:$viewValue}" ' +
+                'typeahead-editable="false" autocomplete="off" placeholder="{{ placeholder }}" />'
+        };
+    }]);
