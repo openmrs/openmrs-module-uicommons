5b08b8de8a477fb087519c81445740cb6b289664
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-10-26 14:58:57 +0530

    UICM-43 - Angular widget for selecting a concept from a short list

---
+++ omod/src/main/webapp/resources/scripts/directives/select-concept-from-list.js
@@ -0,0 +1,50 @@
+angular.module('uicommons.widget.select-concept-from-list', [ 'ui.bootstrap' ])
+
+    .directive('selectConceptFromList', [ function() {
+
+        return {
+            restrict: 'E',
+            scope: {
+                ngModel: '=',
+                id: '@',
+                locale: '@',
+                placeholder: '@',
+                concepts: '&'
+            },
+            link: function($scope, element, attrs) {
+                $scope.required = attrs.hasOwnProperty('required'); // required attribute has no value
+                console.log(attrs);
+                console.log($scope.required);
+                $scope.inputId = emr.domId($scope.id, 'sel-concept', 'input');
+
+                var options = [];
+                _.each($scope.concepts(), function(concept) {
+                    _.each(concept.names, function(name) {
+                        if (emr.compatibleLocaleForSession(name.locale)) {
+                            var display = name.name === concept.display ?
+                                name.name :
+                                name.name + " &rarr; " + concept.display;
+                            options.push({
+                                display: display,
+                                searchOn: name.name,
+                                concept: concept
+                            });
+                        }
+                    });
+                });
+                $scope.options = _.sortBy(options, function(item) {
+                    return item.display.length;
+                });
+
+                $scope.verify = function() {
+                    if(!$scope.ngModel){
+                        $('#'+$scope.inputId).val('');
+                    }
+                }
+            },
+            template: '<input type="text" id="{{ inputId }}" ng-model="ngModel" ng-blur="verify()" ' +
+                'ng-required="required" ' +
+                'typeahead="opt.concept as opt.display for opt in options | filter:{searchOn:$viewValue}" ' +
+                'typeahead-editable="false" autocomplete="off" placeholder="{{ placeholder }}" />'
+        };
+    }]);
