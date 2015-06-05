1650e71f72fee41c6192ecb57e325ff6e1ab3132
mogoodrich <mgoodrich@pih.org>
2015-06-05 09:56:33 -0400

    standard French date translations for bootstrap datepicker to match java date translations

--- omod/src/main/webapp/resources/scripts/datetimepicker/locales/bootstrap-datetimepicker.fr.js
+++ omod/src/main/webapp/resources/scripts/datetimepicker/locales/bootstrap-datetimepicker.fr.js
@@ -4,11 +4,11 @@
  */
 ;(function($){
 	$.fn.datetimepicker.dates['fr'] = {
-		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
-		daysShort: ["dim", "lun", "mar", "mer", "jeu", "ven", "sam", "dim"],
+		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
+		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
 		daysMin: ["d", "l", "ma", "me", "j", "v", "s", "d"],
 		months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
-		monthsShort: ["jan", "fev", "mar", "avr", "mai", "jui", "jul", "aou", "sep", "oct", "nov", "dec"],
+		monthsShort: ["janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
 		today: "Aujourd'hui",
 		suffix: [],
 		meridiem: ["am", "pm"],
@@ -16,3 +16,4 @@
 		format: "dd/mm/yyyy"
 	};
 }(jQuery));
+
