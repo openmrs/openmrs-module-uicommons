f7391d86dae942f6a1aea604b87a31ac52103a94
mogoodrich <mgoodrich@pih.org>
2015-06-05 10:57:04 -0400

    in bootstrap datepicker localization, adding repeating sunday day name back in just in case it was there for a reason

--- omod/src/main/webapp/resources/scripts/datetimepicker/locales/bootstrap-datetimepicker.fr.js
+++ omod/src/main/webapp/resources/scripts/datetimepicker/locales/bootstrap-datetimepicker.fr.js
@@ -4,8 +4,8 @@
  */
 ;(function($){
 	$.fn.datetimepicker.dates['fr'] = {
-		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
-		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
+		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche" ],
+		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim."],
 		daysMin: ["d", "l", "ma", "me", "j", "v", "s", "d"],
 		months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
 		monthsShort: ["janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
