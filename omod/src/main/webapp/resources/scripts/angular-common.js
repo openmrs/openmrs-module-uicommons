3d3693762798646429cb303c5ed7134a1be1b398
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-09-10 16:47:07 +0200

    RA-900 Post review fixes

--- omod/src/main/webapp/resources/scripts/angular-common.js
+++ omod/src/main/webapp/resources/scripts/angular-common.js
@@ -12,39 +12,41 @@ angular.module('uicommons.common', []).
     }).
     
     factory("RestService", function($q) {
-    	return {
-    		getAllResults: function getAllResults(resource, params, deferred, results) {
-    			if (!deferred) {
-    				deferred = $q.defer();
-    			}
+    	function populateResults(resource, params, deferred, results) {
+			resource.query(params).$promise.then(function(res) {
+				results = results.concat(res.results);
+				
+				var hasMoreResults = false;
+				
+				if (res.links) {
+					for (i = 0; i < res.links.length; i++) {
+						if (res.links[i].rel == "next") {
+							var startIndexRe = /startIndex=([0-9]*)/;
+							var startIndex = startIndexRe.exec(res.links[i].uri);
+							params["startIndex"] = startIndex[1];
+							
+							hasMoreResults = true;
+							populateResults(resource, params, deferred, results);
+							
+							break;
+						}
+					}
+				}
+				
+				if (!hasMoreResults) {
+					deferred.resolve(results);
+				}
+			}, function(error) {
+				deferred.reject(error);
+			});
+		}
+    	
+    	return {		
+    		getAllResults: function(resource, params) {
+    			var deferred = $q.defer();
+    			var results = [];
     			
-    			resource.query(params).$promise.then(function(res) {
-    				if (results) {
-    					results = results.concat(res.results);
-    				} else {
-    					results = res.results;
-    				}
-    				
-    				fetchedAll = true;
-    				
-    				if (res.links) {
-    					for (i = 0; i < res.links.length; i++) {
-    						if (res.links[i].rel == "next") {
-    							var startIndexRe = /startIndex=([0-9]*)/;
-    							var startIndex = startIndexRe.exec(res.links[i].uri);
-    							
-    							params["startIndex"] = startIndex[1];
-    							fetchedAll = false;
-    							getAllResults(resource, params, deferred, results);
-    							break;
-    						}
-    					}
-    				}
-    				
-    				if (fetchedAll) {
-    					deferred.resolve(results);
-    				}
-    			});
+    			populateResults(resource, params, deferred, results);
     			
     			return deferred.promise;
     		}
