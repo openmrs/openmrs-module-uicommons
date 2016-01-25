6f49b7ef35b9714d77dd6f0d26ac6209a0d8898d
mogoodrich <mgoodrich@pih.org>
2016-01-25 16:46:56 -0500

    UICM-69: EncounterModel: add participatedIn and createdBy utility methods

--- omod/src/main/webapp/resources/scripts/model/encounter-model.js
+++ omod/src/main/webapp/resources/scripts/model/encounter-model.js
@@ -12,6 +12,33 @@
 
         canBeEditedBy: function(userModel) {
             return userModel.hasPrivilege("Task: emr.patient.encounter.edit");
+        },
+
+        createdBy: function(userModel) {
+
+            var creator;
+
+            if (this.creator) {
+                creator = this.creator;
+            }
+            else if (this.auditInfo && this.auditInfo.creator) {
+                creator  = this.auditInfo.creator;
+            }
+
+            if (!creator) {
+                return false; // if we don't know the creator, we can't answer, so default to false
+            }
+
+            return creator && creator.uuid == userModel.uuid;
+        },
+
+        participatedIn: function(provider) {
+            if (!provider.uuid) {
+                return false;
+            }
+            return _.find(this.encounterProviders, function(p) {
+                return p.provider && p.provider.uuid == provider.uuid;
+            })
         }
     }
 
