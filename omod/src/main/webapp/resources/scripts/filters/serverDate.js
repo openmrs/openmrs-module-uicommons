1be34c912fbea6f91e3e5fb270c72d45b2840919
Brandon Istenes <bistenes@gmail.com>
2021-09-09 11:54:02 -0700

    Clarify comment on serverDate filter and add a console warning (#97)
    * Clarify comment on serverDate filter and add a console warning
    
    * Improve message

--- omod/src/main/webapp/resources/scripts/filters/serverDate.js
+++ omod/src/main/webapp/resources/scripts/filters/serverDate.js
@@ -1,11 +1,18 @@
 angular.module('uicommons.filters').
 
     /*
-     * Angular's "date" filter displays the date in the client's timezone. This filter assumes that the ISO-formatted
-     * time string passed in is in the server's timezone, and drops timezone information before formatting it.
+     * This is a function that takes an ISO date string, blindly removes its timezone
+     * suffix, and formats it in a way that is neither locale-specific nor is
+     * correctly interpreted by the JavaScript `Date` constructor (the year comes
+     * out negative in some browsers; e.g. `new Date("08-Sep-2021") === new Date(-2021, 8, 8)` in Firefox).
+     *
+     * Its intended use case is to format a Date for display with respect to the server's
+     * time zone. This is better accomplished using a function like
+     * [Date.prototype.toLocaleString](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString).
      */
     filter('serverDate', ['$filter', function($filter) {
         return function(isoString, format) {
+            console.warn("Use of the `serverDate` filter is likely to cause problems. Please consider using Date.prototype.toLocaleString instead: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString");
             if (!isoString) {
                 return null;
             }
