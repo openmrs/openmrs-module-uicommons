fd5a56a00137b5f63e7213c06c27cec669af8ad8
Brandon Istenes <bistenes@gmail.com>
2021-09-09 11:12:04 -0700

    Improve message

--- omod/src/main/webapp/resources/scripts/filters/serverDate.js
+++ omod/src/main/webapp/resources/scripts/filters/serverDate.js
@@ -2,9 +2,13 @@ angular.module('uicommons.filters').
 
     /*
      * This is a function that takes an ISO date string, blindly removes its timezone
-     * suffix, and formats it in a way that is neither locale-specific nor can be
+     * suffix, and formats it in a way that is neither locale-specific nor is
      * correctly interpreted by the JavaScript `Date` constructor (the year comes
-     * out negative; e.g. `new Date("08-Sep-2021") === new Date(-2021, 8, 8)`).
+     * out negative in some browsers; e.g. `new Date("08-Sep-2021") === new Date(-2021, 8, 8)` in Firefox).
+     *
+     * Its intended use case is to format a Date for display with respect to the server's
+     * time zone. This is better accomplished using a function like
+     * [Date.prototype.toLocaleString](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString).
      */
     filter('serverDate', ['$filter', function($filter) {
         return function(isoString, format) {
