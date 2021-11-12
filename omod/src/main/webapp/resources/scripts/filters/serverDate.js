1d7ee03970534bf48a18006c3a17226beb82184c
Brandon Istenes <bistenes@gmail.com>
2021-11-12 09:42:47 -0800

    Put date format functions in their own filter file

--- omod/src/main/webapp/resources/scripts/filters/serverDate.js
+++ omod/src/main/webapp/resources/scripts/filters/serverDate.js
@@ -44,24 +44,6 @@ angular.module('uicommons.filters').
         }
     }])
 
-    .filter('toLocaleString', ['locale', function(locale) {
-        return function(date, options) {
-            return new Date(date).toLocaleString(locale, options);
-        }
-    }])
-
-    .filter('toLocaleDateString', ['locale', function(locale) {
-        return function(date, options) {
-            return new Date(date).toLocaleDateString(locale, options);
-        }
-    }])
-
-    .filter('toLocaleTimeString', ['locale', function(locale) {
-        return function(date, options) {
-            return new Date(date).toLocaleTimeString(locale, options);
-        }
-    }])
-
     .filter('serverDateForRESTSubmit', ['$filter', function($filter) {
         return function(isoString) {
             return $filter('serverDate')(isoString, "YYYY-MM-DDTHH:mm:ss.SSS");
